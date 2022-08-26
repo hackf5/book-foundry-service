74246861542a84688b6a2aa0f7762831c9d3cb52
Brian Tyler <brian@hackf5.io>
2022-08-26 19:36:26 +0100

    feat: add create entry

--- src/HackF5.BookFoundry.Service.WebApi/GraphQL/BookMutation.cs
+++ src/HackF5.BookFoundry.Service.WebApi/GraphQL/BookMutation.cs
@@ -41,6 +41,35 @@ public class BookMutation
         }
     }
 
+    public async Task<CreateEntryOutput> CreateEntryAsync(
+        CreateEntryInput input,
+        [Service] ApplicationDbContext ctx,
+        IResolverContext context,
+        CancellationToken cancellation)
+    {
+        try
+        {
+            var book = await ctx.Books.FirstAsync(x => x.Id == input.BookId, cancellation);
+
+            var entry = ctx.Entries.AddProxy();
+            entry.Name = input.Name;
+            entry.Index = book.Entries.Max(x => x.Index) + 1;
+            entry.Book = book;
+
+            var revision = ctx.Revisions.AddProxy();
+            revision.Entry = entry;
+            revision.Text = string.Empty;
+
+            await ctx.SaveChangesAsync(cancellation);
+
+            return new(entry.Id);
+        }
+        catch (Exception ex)
+        {
+            throw QueryMessageException.New("An error occurred while creating the entry.", ex, context);
+        }
+    }
+
     public async Task<CreateRevisionOutput> CreateRevisionAsync(
         CreateRevisionInput input,
         [Service] ApplicationDbContext ctx,
---
+++ src/HackF5.BookFoundry.Service.WebApi/GraphQL/Model/CreateEntryOutput.cs
@@ -0,0 +1,3 @@
+namespace HackF5.BookFoundry.Service.WebApi.GraphQL.Model;
+
+public record CreateEntryOutput(int EntryId);
